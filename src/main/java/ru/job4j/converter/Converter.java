204c56bd9fef8157f93d93de41a600b642876bbf
BoykoKatty <grania@list.ru>
2023-05-09 18:09:38 +0300

    Converter test

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -10,4 +10,17 @@ public class Converter {
         return value / 60;
     }
 
+    public static void main(String[] args) {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.printf("%s rubles are %s euro. Test result : %s%n", in, out, passed);
+        in = 180;
+        expected = 3;
+        out =  Converter.rubleToDollar(in);
+        passed = expected == out;
+        System.out.printf("%s rubles are %s dollars. Test result : %s%n", in, out, passed);
+    }
+
 }
