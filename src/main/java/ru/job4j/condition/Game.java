a76214d0fc1391be9a235b08405e18cbc2a3521d
BoykoKatty <grania@list.ru>
2023-05-14 16:50:37 +0300

    Game

---
+++ src/main/java/ru/job4j/condition/Game.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class Game {
+    public static void menu(String name) {
+        if ("super mario".equals(name)) { /* сравниваем переменную name с "super mario" */
+            System.out.println("Start - super mario");
+        } else if ("tanks".equals(name)) { /* Сравнить переменную name с "tanks" */
+            System.out.println("Start - tanks");
+        } else if ("tetris".equals(name)) { /* Сравнить переменную name с "tetris" */
+            System.out.println("Start - tetris");
+        }
+    }
+
+    public static void main(String[] args) {
+        Game.menu("tanks");
+    }
+}
