39c4ddc61402f52275dd40007070334acae2597a
BoykoKatty <grania@list.ru>
2023-05-14 16:46:47 +0300

    Main

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -3,5 +3,9 @@ package ru.job4j;
 public class Main {
     public static void main(String[] args) {
         System.out.println("H-e-l-l-o-,-f-r-i-e-n-d!");
+        String a = "Hello";
+        String b = "Hel";
+        b  += "lo";
+        System.out.println(a == b);
     }
 }
